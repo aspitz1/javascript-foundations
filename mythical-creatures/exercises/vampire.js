9c8107fb0d4778801df6d04840aacea01f90c0b3
Anna Spitz <aspitzemail@gmail.com>
2022-05-16 20:42:49 -0500

    Add finished vampire

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,20 @@
+class Vampire {
+  constructor(name, pet) {
+    this.name = name
+    this.pet = pet || 'bat'
+    this.thirsty = true
+    this.ouncesDrank = 0
+  }
+  drink() {
+    if(this.ouncesDrank < 50) {
+    this.ouncesDrank = this.ouncesDrank + 10
+    }
+    if(this.ouncesDrank === 10){
+      this.thirsty = !this.thirsty
+    } else if(this.ouncesDrank >= 50) {
+      return 'I\'m too full to drink anymore!'
+    }
+  }
+}
+
+module.exports = Vampire
